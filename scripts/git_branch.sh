9b5f6ab93885117e416a8ac9d007213c596432d3
Tim Ngeno <timngeno424@gmail.com>
2026-05-07 09:06:15 +0300

    update pills styling

--- scripts/git_branch.sh
+++ scripts/git_branch.sh
@@ -11,5 +11,6 @@ branch=$(git -C "$DIR" symbolic-ref --short HEAD 2>/dev/null)
 [ ${#branch} -gt 24 ] && branch="${branch:0:22}…"
 
 # colour5 = pywal magenta/purple
-printf '#[fg=colour5,bg=default]\ue0b6#[fg=colour0,bg=colour5,bold]󰘬 #[fg=colour5,bg=colour0]\ue0b0#[fg=colour15,bg=colour0] %s #[fg=colour0,bg=default]\ue0b4#[default] ' \
+printf '#[fg=colour5,bg=default]#[fg=colour0,bg=colour5,bold]󰘬 %s#[fg=colour5,bg=default]#[default] ' \
     "$branch"
+
--- scripts/session_pill.sh
+++ scripts/session_pill.sh
@@ -11,5 +11,5 @@ COLOR=$(grep "^${SESSION} " "$MAP" 2>/dev/null | awk '{print $2}')
 COLOR=${COLOR:-4}
 
 # \ue0b6 = left round cap, \ue0b4 = right round cap
-printf '#[fg=colour%s,bg=default]\ue0b6#[fg=colour0,bg=colour%s,bold]󰌆 #[fg=colour%s,bg=colour0]\ue0b0#[fg=colour15,bg=colour0] %s #[fg=colour0,bg=default]\ue0b4#[default] ' \
-    "$COLOR" "$COLOR" "$COLOR" "$SESSION"
+printf '#[fg=colour%s,bg=default]#[fg=colour0,bg=colour%s,bold]󰌆  %s#[fg=colour%s,bg=default]#[default] ' \
+    "$COLOR" "$COLOR" "$SESSION" "$COLOR"
